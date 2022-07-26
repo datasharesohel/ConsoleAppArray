d5e89ec70240a5c04995ed814f76b6e25396c0d0
Sohel <datasharesohel@gmail.com>
2022-07-26 23:51:54 +0600

    First Change

---
+++ ConsoleAppArray/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArray
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+   
+}
---
+++ ConsoleAppArray/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var names = new[]
+            {
+                 new {name="Jhon",Age=50},
+                 new {name="Bill",Age=40},
+                 new {name="Farid",Age=30},
+            };
+            foreach (var item in names)
+            {
+                Console.WriteLine($"Name:{item.name }, Age: {item.Age }");
+                //Console.WriteLine(item.Age);
+            }            
+            
+            Console.WriteLine("------------");
+            Console.WriteLine("-------Array Example-----");
+
+            Person[] family = new[]
+            {
+                new Person("Jhon",53),
+                new Person("Marry",45),
+                new Person("Dolly",20)
+            };
+
+            foreach (var item in family)
+            {
+                Console.WriteLine($"Name:{item.Name }, Age: {item.Age }");
+               
+            }
+
+            Console.WriteLine("-------List Example -----");
+            List<Person> family1 = new List<Person>()
+            {
+                new Person("Jhon",53),
+                new Person("Marry",45),
+                new Person("Dolly",20)
+
+            };
+            foreach (var item in family1)
+            {
+                Console.WriteLine($"Name:{item.Name }, Age: {item.Age }");
+
+            }
+
+          
+
+        }
+    }
+}
